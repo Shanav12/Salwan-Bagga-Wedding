03ff51da07627725f5f1c7b577c4c840e8f17171
Shanav12 <bagga.shan@gmail.com>
2026-02-04 18:27:39 -0600

    Setting up React app

---
+++ my-app/src/components/HomePage.jsx
@@ -0,0 +1,15 @@
+import pic1 from "../assets/sahilambika1.jpg"
+
+const HomePage = () => {
+    return (
+        <>
+            <div>
+                Welcome to the Salwan-Bagga Wedding!
+                <img src={pic1} alt="Engagement"/>
+            </div>
+        </>
+    )
+}
+
+
+export default HomePage;
