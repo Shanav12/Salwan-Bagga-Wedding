06881de65a15ab7b9a50242e6a220861f2a357bd
Shanav12 <bagga.shan@gmail.com>
2026-02-06 23:34:55 -0600

    Adding placeholder div to fun facts page to maintain consistent background style

--- app/src/components/FunFacts.jsx
+++ app/src/components/FunFacts.jsx
@@ -1,5 +1,10 @@
-const FunFacts = () => {
 
+const FunFacts = () => {
+    return (
+        <div className="justify-center min-h-screen bg-gradient-to-b from-rose-50 to-cream-50 bg-[#fdfbf7]">
+
+        </div>
+    )
 }
 
 export default FunFacts;
