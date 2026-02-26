874e39048f751abc78336327127fe77ecfc1299e
Shanav12 <bagga.shan@gmail.com>
2026-02-26 00:35:24 -0600

    Compressing save the date

--- app/src/components/WeddingLogistics.jsx
+++ app/src/components/WeddingLogistics.jsx
@@ -2,7 +2,7 @@ import "../App.css"
 import Confetti from 'react-confetti'
 import { useWindowSize } from 'react-use'
 import { useState, useEffect } from "react"
-import saveTheDateBack from "../assets/saveTheDateBack.png"
+import saveTheDateBack from "../assets/saveTheDateBack.webp"
 
 
 const WeddingLogistics = () => {
